dad0981354d464b7b9b13d341fd6877353fca84d
Elliott Mantock <emantock@gmail.com>
2026-06-06 14:29:59 -0700

    fix(ui): adjust responsive margins for Container component

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -5,7 +5,7 @@ export const Container = ({ className, ...props }: ComponentProps<'div'>) => {
   return (
     <div
       className={cn(
-        'max-w-full xl:mx-64 lg:mx-32 sm:mx-8 mx-2 lg:mt-16 mt-8 mb-8',
+        'max-w-full 2xl:mx-64 xl:mx-48 lg:mx-32 sm:mx-8 mx-3 lg:mt-16 mt-8 mb-8',
         className,
       )}
       {...props}
